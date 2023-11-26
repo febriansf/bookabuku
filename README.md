88dd4a34e72279a63128c3f7c33219155f2c567a
Febrian SF <febriansf@student.uns.ac.id>
2023-11-26 12:44:42 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bookabuku
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/bookabuku/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bookabuku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
