59d9b2fa7df10da49e1caa8bb29fdb9a297e2c7e
RizkyAkb <rawp58@gmail.com>
2023-12-10 14:26:37 +0700

    add some page
    search page, shelf page, detail book page

--- lib/components/cari.dart
+++ lib/components/cari.dart
@@ -10,12 +10,46 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: 16.0,
-        vertical: 20.0,
+    return Center(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(21),
+            child: SearchBar(
+              elevation: MaterialStateProperty.all(0.0),
+              side: MaterialStateProperty.all(
+                  const BorderSide(color: Colors.blue)),
+              hintText: 'Cari buku',
+              hintStyle: MaterialStateProperty.all(
+                  const TextStyle(color: Colors.grey)),
+              trailing: [
+                const Icon(
+                  Icons.search,
+                  color: Colors.blue,
+                ),
+              ],
+              onSubmitted: (String value) {
+                print('value: $value');
+              },
+            ),
+          ),
+          Center(
+            child: Text(
+              (() {
+                if (true) {
+                  return "Maaf buku yang kamu cari tidak tersedia";
+                } else {
+                  return "Buku tidak ada";
+                }
+              })(),
+              style: TextStyle(
+                  fontSize: 16.0,
+                  // fontWeight: FontWeight.bold,
+                  color: Color(0xFF3EC6FF)),
+            ),
+          )
+        ],
       ),
-      child: Text('CARI'),
     );
   }
 }
---
+++ lib/components/detail_buku.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class BookDetailPage extends StatefulWidget {
+  const BookDetailPage({super.key});
+
+  @override
+  State<BookDetailPage> createState() => _BookDetailPageState();
+}
+
+class _BookDetailPageState extends State<BookDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: IconButton(
+                onPressed: () {
+                  (() {},);
+                },
+                icon: Icon(
+                  Icons.chevron_left,
+                  color: Colors.blue,
+                  size: 35,
+                )),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.height * 0.5,
+            // color: Colors.amber,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  // alignment: Alignment.topCenter,
+                  margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  // color: Color(0xFF3EC6FF),
+                  child: Card(
+                    elevation: 0,
+                    semanticContainer: true,
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    child: Image.network(
+                      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1639163872l/58293924.jpg',
+                      fit: BoxFit.contain,
+                    ),
+                    // color: Color(0xFF3EC6FF),
+                  ),
+                ),
+                Text("Lorem Ipsum",
+                    style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF3EC6FF))),
+                Text("Dolor Sit Amet",
+                    style: TextStyle(
+                        fontSize: 15.0,
+                        // fontWeight: FontWeight.bold,
+                        color: Color(0xFF3EC6FF))),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(24, 24, 24, 0),
+            width: MediaQuery.of(context).size.width,
+            height: 70,
+            // color: Colors.blue,
+            child: Card(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("bookPages"),
+                      Text(
+                        "212",
+                        style: TextStyle(
+                            color: Color(0xFF3EC6FF),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      )
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("bookGenre"),
+                      Text(
+                        "Horor",
+                        style: TextStyle(
+                            color: Color(0xFF3EC6FF),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      )
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("bookRating"),
+                      Text(
+                        "4,9/5",
+                        style: TextStyle(
+                            color: Color(0xFF3EC6FF),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(24),
+            child: Text(
+              "The Bottom Navigation bar has become popular in the last few years for navigation between different UI. Many developers use bottom navigation because most of the app is available now using this widget for navigation between different screens.The bottom navigation bar in Flutter can contain multiple items such as text labels, icons, or both. It allows the user to navigate between the top-level views of an app quickly. If we are using a larger screen, it is better to use a side navigation bar.In Flutter application, we usually set the bottom navigation bar in conjunction with the scaffold widget. Scaffold widget provides a Scaffold.bottomNavigationBar argument to set the bottom navigation bar. It is to note that only adding BottomNavigationBar will not display the navigation items. It is required to set the BottomNavigationItems for Items property that accepts a list of BottomNavigationItems widgets.Remember the following key points while adding items to the bottom navigation bar:We can display only a small number of widgets in the bottom navigation that can be 2 to 5.It must have at least two bottom navigation items. Otherwise, we will get an error.It is required to have the icon and title properties, and we need to set relevant widgets for them.",
+              style: TextStyle(
+                fontSize: 15.0,
+                // fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.justify,
+            ),
+          ),
+          TextField(
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+          )
+        ],
+      ),
+    );
+  }
+}
